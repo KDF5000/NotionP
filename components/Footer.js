2c0c6a9aeb8dcd195721ac5523bac21db48e0179
kongdefei <kongdefei@bytedance.com>
2023-09-24 10:43:00 +0800

    modify footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Header({ children, home }) {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                <p className={styles.message}>Inspire Creativity,&nbsp;Always Day 1</p>
+                <p className={styles.message}>Stay Curious,&nbsp;Always Day 1</p>
                 <p className={styles.copyright}>Powered by <a href='http://github.com/KDF5000/NotionP'>NotionP</a>&nbsp;&nbsp;|&nbsp;&nbsp;Copyright © 2023 Pcursor</p>
             </div>
         </footer>
